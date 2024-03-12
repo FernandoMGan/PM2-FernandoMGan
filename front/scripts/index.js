d9fd462e8b16d83d0f4aa84c83781b4a7171b21b
Fernando Gan <fmgsch@gmail.com>
2024-03-12 12:59:20 -0300

    Home Work M2 - Pelis Pedia - Entregable Ok

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,13 +1,15 @@
+// Se importan los módulos necesarios
 const { Progreso } = require('./progressBar.js');
 const { RepoMovies } = require('./repoMovies.js');
 const { getFakeStoreProducts } = require('./prmPeliculas.js');
 const { tstAynsAwait } = require('./prmTestAsyncAway.js');
 const { lsp } = require('./index-abaut.js');
 
-
+// Se obtiene la referencia al contenedor de la barra de progreso
 const progBarDraw = document.getElementById('prog-bar-cntd');
 progBarDraw.classList.remove('hidden');
 
+// Se obtienen las películas ficticias del servidor
 getFakeStoreProducts().then(dbPeliculas => {
     const ProgBarRef = {
         "progBarCntd": document.getElementById('prog-bar-cntd'),
@@ -20,33 +22,27 @@ getFakeStoreProducts().then(dbPeliculas => {
         "valorInicial": 1,
     };
 
-    // console.log(`Devolviendo Base de peliculas desde index.js :> Items Tot ( ${dbPeliculas.length} ) y Lista :> `, dbPeliculas);
     const progbar = new Progreso(ProgBarRef, null, 1, dbPeliculas.length);
     const repMovies = new RepoMovies();
 
-    // Limpieza y carga de peliculas
     repMovies.limpiarTarjetas();
     bucle(0, dbPeliculas, progbar, progBarDraw);
 })
-    .catch(error => {
-        // console.log(`Devolviendo Base de peliculas desde index.js :> Items Tot ( ${dbPeliculas.length} ) y Lista :> `, dbPeliculas);
-    });
+.catch(error => {
+    console.error('Error al cargar las películas:', error);
+});
 
-tstAynsAwait
+// Llamada a la función para probar async/await
+tstAynsAwait();
+
+// Llamada a la función lsp
 lsp();
 
-// Fin de Codigo ---------------------------------------------
-
+// Función para crear las tarjetas de las películas
 function bucle(index, dbPeliculas, progbar, progBarDraw) {
     if (index < dbPeliculas.length) {
         const movieData = dbPeliculas[index];
-
-        // alert('Get Fake Index id  ' + dbPeliculas[index]._id);
-        // console.log(`Mostrar Peli posicion ${index} , con Titulo ${dbPeliculas[index].title} y con Ratio ${dbPeliculas[index].rate}`);
-
-        // repMovies.createMovie(movieData);
         CreateDivs(movieData);
-
         setTimeout(() => {
             bucle(index + 1, dbPeliculas, progbar, progBarDraw);
             progbar.ProgresoReflejo(index);
@@ -56,17 +52,17 @@ function bucle(index, dbPeliculas, progbar, progBarDraw) {
     }
 }
 
+// Función para crear las tarjetas de las películas
 function CreateDivs(pelicula) {
     const contenedorTarjetas = document.getElementById('cntd-trjs-ind');
-
     const tarjeta = document.createElement("div");
     tarjeta.classList.add("cntd-trjs-ind");
 
-    // 1. Crear un div contenedor
+    // Crear el contenedor del título y el botón de favorito
     const contenedorTituloYFavorito = document.createElement("div");
     contenedorTituloYFavorito.classList.add("titulo-y-favorito");
 
-    // Agregar el botón de favorito (corazón) al div contenedor
+    // Crear el botón de favorito
     const corazon = document.createElement("span");
     corazon.classList.add("bi", "bi-heart", "fs-2", "text-danger", "float-end", "me-5");
     corazon.setAttribute("data-favorito", "false");
@@ -76,101 +72,69 @@ function CreateDivs(pelicula) {
             corazon.classList.remove("bi-heart-fill");
             corazon.classList.add("bi-heart");
             corazon.setAttribute("data-favorito", "false");
-            favHeartToggle(Event, false);
         } else {
             corazon.classList.remove("bi-heart");
             corazon.classList.add("bi-heart-fill");
             corazon.setAttribute("data-favorito", "true");
-            favHeartToggle(Event, true);
         }
     });
     contenedorTituloYFavorito.appendChild(corazon);
 
-    // 2. Agregar el título de la película dentro del div contenedor
+    // Crear el enlace del título de la película
     const titulo = document.createElement("div");
-    titulo.classList.add("trj-ind");
-    titulo.classList.add("trj-title");
-    titulo.innerHTML = `<h5>${pelicula.title}</h5>`;
+    titulo.classList.add("trj-ind", "trj-title");
+    const enlaceTitulo = document.createElement("a");
+    enlaceTitulo.href = "#";
+    enlaceTitulo.innerHTML = `<h5>${pelicula.title}</h5>`;
+    titulo.appendChild(enlaceTitulo);
     contenedorTituloYFavorito.appendChild(titulo);
-
-    // Agregar el div contenedor a la tarjeta
     tarjeta.appendChild(contenedorTituloYFavorito);
 
+    // Crear la imagen de la película
     const imagen = document.createElement("div");
-    imagen.classList.add("trj-ind");
-    imagen.classList.add("trj-imagen");
+    imagen.classList.add("trj-ind", "trj-imagen");
     imagen.innerHTML = `<img src="${pelicula.poster}" alt="Imagen de ${pelicula.title}">`;
     tarjeta.appendChild(imagen);
 
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-    // Crear el contenedor flex para la director
+    // Crear el contenedor flex para el director
     const cntdFlexDirector = document.createElement("div");
     cntdFlexDirector.classList.add("contenedor-flex");
-
     const director = document.createElement("div");
-    director.classList.add("trj-ind");
-    director.classList.add("trj-ind-titdet");
-    director.innerHTML = `<h6> director </h6>`;
+    director.classList.add("trj-ind", "trj-ind-titdet");
+    director.innerHTML = `<h6>Director</h6>`;
     cntdFlexDirector.appendChild(director);
-
     const directorDsc = document.createElement("div");
-    directorDsc.classList.add("trj-ind");
-    directorDsc.classList.add("trj-ind-director");
+    directorDsc.classList.add("trj-ind", "trj-ind-director");
     directorDsc.innerHTML = `<h5>${pelicula.director}</h5>`;
     cntdFlexDirector.appendChild(directorDsc);
-
-    // Agregar el contenedor flex para la calificación a la tarjeta
     tarjeta.appendChild(cntdFlexDirector);
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-    // Crear el contenedor flex para la duracion
+    // Crear el contenedor flex para la duración
     const cntdFlexDuration = document.createElement("div");
     cntdFlexDuration.classList.add("contenedor-flex");
-
     const duracion = document.createElement("div");
-    duracion.classList.add("trj-ind");
-    duracion.classList.add("trj-ind-titdet");
-    duracion.innerHTML = `<h6> Calificacion </h6>`;
+    duracion.classList.add("trj-ind", "trj-ind-titdet");
+    duracion.innerHTML = `<h6>Duración</h6>`;
     cntdFlexDuration.appendChild(duracion);
-
     const duracionTime = document.createElement("div");
-    duracionTime.classList.add("trj-ind");
-    duracionTime.classList.add("trj-ind-duration");
+    duracionTime.classList.add("trj-ind", "trj-ind-duration");
     duracionTime.innerHTML = `<h5>${pelicula.duration}</h5>`;
     cntdFlexDuration.appendChild(duracionTime);
-
-    // Agregar el contenedor flex para la calificación a la tarjeta
     tarjeta.appendChild(cntdFlexDuration);
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
     // Crear el contenedor flex para la calificación
     const cntdFlexRate = document.createElement("div");
     cntdFlexRate.classList.add("contenedor-flex");
-
     const calificacion = document.createElement("div");
-    calificacion.classList.add("trj-ind");
-    calificacion.classList.add("trj-ind-titdet");
-    calificacion.innerHTML = `<h6> Calificacion </h6>`;
+    calificacion.classList.add("trj-ind", "trj-ind-titdet");
+    calificacion.innerHTML = `<h6>Calificación</h6>`;
     cntdFlexRate.appendChild(calificacion);
-
     const rate = document.createElement("div");
-    rate.classList.add("trj-ind");
-    rate.classList.add("trj-ind-Rate");
+    rate.classList.add("trj-ind", "trj-ind-Rate");
     const rateValue = parseFloat(pelicula.rate.$numberDecimal); // Convertir a número
     rate.innerHTML = `<h3>${rateValue}</h3>`;
     cntdFlexRate.appendChild(rate);
-
-    // Agregar el contenedor flex para la calificación a la tarjeta
     tarjeta.appendChild(cntdFlexRate);
-    // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-    // Agregar la tarjeta al contenedor de tarjetas
     contenedorTarjetas.appendChild(tarjeta);
-}
-
-function favHeartToggle(event,vdada, vflag) {
-    console.log('Event :>> en Flag :>> ', vflag + ' \n La data :>> ' + vdada + ' \n La bandera' + event );
 }
